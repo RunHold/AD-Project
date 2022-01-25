964a4efff7d61363da7b380d3f663975c1c92971
RunHold <badem922@gmail.com>
2022-01-25 12:22:31 +0300

    16.4

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+  <v-navigation-drawer app v-model="drawer">
+    <v-list-item>
+      <v-list-item-content>
+            <v-list-item-title class="title">
+              <v-img
+                src = "https://yt3.ggpht.com/ytc/AKedOLRCvhZh9AZaL_nN3a78Wqw7WUT9y_WYq2CN5fYV=s900-c-k-c0x00ffffff-no-rj"
+                >
+              </v-img>
+              КИПУ
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Учебный проект
+            </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list dense>
+        <v-list-item-group color="indigo lighten-1">
+          <v-list-item
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ link.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer> 
+  <v-app-bar app dark color="indigo lighten-1">
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>
+      <router-link 
+      to="/" 
+      tag="span" 
+      class="pointer">
+      Home
+    </router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn 
+       v-for="link in links"
+       :key="link.title"
+       :to="link.url" 
+       text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+  <v-main>
+    <router-view></router-view> 
+  </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      drawer: false,
+      links:[
+      {title:"Login", icon:"mdi-lock", url:"/login"},
+      {title:"Registration",icon:"mdi-book-open-variant",url:"/registration"},
+      {title:"Orders",icon:"mdi-bookmark-multiple-outline",
+      url:"/orders"},
+      {title:"New ad", icon:"mdi-math-compass", url:"/new"},
+      {title:"My ads", icon:"mdi-account-group", url:"/list"}
+      ]
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,49 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App.vue'
-
+import vuetify from './plugins/vuetify'
+import router from './router/index'
+import store from './store'
+//import fb from 'firebase'
+//import firebase from 'firebase/app'
+import fb from 'firebase/app'
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+Vue.use(Router)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+    render: h => h(App),
+  router: router,
+  store,
+  created(){
+  const firebaseConfig = {
+      apiKey: "AIzaSyC8DTr2EQB5Ks6x7E6--qOSAqkFRzW3ehQ",
+      authDomain: "add-pro-acc8d.firebaseapp.com",
+      projectId: "add-pro-acc8d",
+      storageBucket: "add-pro-acc8d.appspot.com",
+      messagingSenderId: "775797943383",
+      appId: "1:775797943383:web:95b327f3604463dc2bc712",
+      measurementId: "G-NFWKMBDEQB"
+  };
+  // Initialize Firebase
+  fb.initializeApp(firebaseConfig);
+  fb.analytics();
+  //fb.auth().onAuthStateChanged(user => {
+    //здесь можно обновить пользователя в store
+    //console.log(user)
+  //});
+  fb.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log(`Смотрим что мы получили: ${user.uid}`)
+      this.$store.dispatch('autoLoginUser', user.uid)
+    }
+ })
+
+  //const app = initializeApp(firebaseConfig);
+  //getAnalytics(app);
+
+  this.$store.dispatch('fetchAds')
+}
 }).$mount('#app')
